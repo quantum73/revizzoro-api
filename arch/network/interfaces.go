cc4ccad9bc38d0d088f34669f410fd84aa158267
Nikita Fomin <fominquant@yandex.ru>
2024-12-28 15:29:01 +0300

    Add DTO interface

--- arch/network/interfaces.go
+++ arch/network/interfaces.go
@@ -1,6 +1,9 @@
 package network
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
 
 type ApiError interface {
 	GetCode() int
@@ -17,4 +20,9 @@ type Response interface {
 
 type BaseController interface {
 	MountRoutes(group *gin.RouterGroup)
+}
+
+type Dto[T any] interface {
+	GetValue() *T
+	ValidateErrors(errs validator.ValidationErrors) ([]string, error)
 }
